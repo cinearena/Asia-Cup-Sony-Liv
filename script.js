226aee5f1cd0d5ae76c6978103727ed4ce72167e
cinearena <asifansarieng@gmail.com>
2025-09-21 13:21:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,81 +1,15 @@
-
-        // Disable right-click and hotkeys
-        document.addEventListener("contextmenu", function(e) {
-            e.preventDefault();
-        });
-
-        document.addEventListener("keydown", function(e) {
-            if (
-                e.key === "F12" ||
-                (e.ctrlKey && e.shiftKey && (e.key === "I" || e.key === "J")) ||
-                (e.ctrlKey && e.key === "U")
-            ) {
-                e.preventDefault();
-            }
-        });
-
-        // DevTools Detection - Attempt to close tab
-        (function() {
-            let devtoolsOpen = false;
-            const element = new Image();
-
-            Object.defineProperty(element, 'id', {
-                get: function () {
-                    devtoolsOpen = true;
-                    throw new Error("DevTools detected");
-                }
-            });
-
-            setInterval(function () {
-                devtoolsOpen = false;
-                console.dir(element);
-                if (devtoolsOpen) {
-                    alert("Developer tools are not allowed!");
-                    window.open('', '_self').close(); // Attempts to close the tab
-                }
-            }, 1000);
-        })();
-   
-
-
-      function getParams() {
-        const url = new URL(window.location.href);
-        const [mpdUrlPart, drmPart] = url.search.slice(1).split('|');
-        const mpdUrl = decodeURIComponent(mpdUrlPart);
-        const searchParams = new URLSearchParams(drmPart);
-
-        const drmScheme = searchParams.get("drmScheme");
-        const drmLicense = searchParams.get("drmLicense"); // Format: keyId:key
-
-        let keyId = "", key = "";
-        if (drmLicense) {
-          [keyId, key] = drmLicense.split(':');
-        }
-
-        return {
-          file: mpdUrl,
-          drm: {
-            scheme: drmScheme,
-            keyId,
-            key,
-          },
-        };
-      }
-
-      const { file, drm } = getParams();
-
-      jwplayer("jwplayerDiv").setup({
-        file: file,
-        type: "dash",
-        drm: {
-          [drm.scheme]: {
-            keyId: drm.keyId,
-            key: drm.key,
-          },
-        },
-        autostart: true,
-        mute: false,
-        image: "WhatsApp Image 2025-02-22 at 5.19.53 PM.jpeg"
-      });
-   
-
+jwplayer("jwplayerDiv").setup({    
+     file: "https://abfdm2saaaaaaaamljqrv7ybrrx7h.47348d5020154bfaac6dd4418cd23c5d.emt.cf.ww.aiv-cdn.net/pdx-nitro/live/clients/dash/enc/nmcsyv34ql/out/v1/663f8f08059340889951eee1351eddc2/cenc.mpd", 
+         position: 'bottom',  
+        "autostart": true,
+        "Volume": "100",
+        "mute": false,
+        "stretching": "exactfit",
+        "width": "100%", 
+      type: "dash",    
+      drm: { "clearkey": {    
+               "keyId": "cec3ed49d57d515346b5a94787d47c56",    
+              "key": "2eb7e6bce0c1dd2d17f754092e24cf39" 
+            }   
+             }               
+});   
